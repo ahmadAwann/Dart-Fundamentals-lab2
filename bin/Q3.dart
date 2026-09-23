7c1ce6012fc52c2c0681286a4b60b3cc2791cc36
ahmadAwann <ahmadmuniro280@gmail.com>
2026-09-23 14:20:23 +0500

    Add Q1,Q2 & Q3

--- bin/Q3.dart
+++ bin/Q3.dart
@@ -3,6 +3,12 @@ void main(){
   var list = [(name:"Zia",age:24),(name:"Hashim",age:17),(name:"Ahmed",age:21),(name:"Saif",age:22),(name:"Mudasir",age:22)];
 
   list.sort((a,b)=>a.age.compareTo(b.age));
+  print("Before Sorting:");
+  for (var item in list) {
+      print("Name: ${item.name}, Age : ${item.age}");
+  }
+  print("-----------------");
+  print("After Sorting");
   for (var item in list) {
       print("Name: ${item.name}, Age : ${item.age}");
   }
